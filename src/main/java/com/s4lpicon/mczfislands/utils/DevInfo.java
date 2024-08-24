2794a02ad4f2e515e0c64cdd0c9a39e776f8db0e
Jeferson Pinzón <S4LPICONG4MER3993@GMAIL.COM>
2024-08-24 17:43:42 -0500

    mejor info de desarrollo

--- src/main/java/com/s4lpicon/mczfislands/utils/DevInfo.java
+++ src/main/java/com/s4lpicon/mczfislands/utils/DevInfo.java
@@ -1,11 +1,15 @@
 package com.s4lpicon.mczfislands.utils;
 
+import com.s4lpicon.mczfislands.objets.Island;
 import org.bukkit.entity.Player;
 
 import static com.s4lpicon.mczfislands.islandsmanager.IslandsManager.activeIslands;
 
 public class DevInfo {
     public static void devinfo(Player player){
-        player.sendMessage("Info"+ activeIslands.toString());
+        for (Island island : activeIslands.values()){
+            player.sendMessage(island.toString());
+            player.sendMessage("------------------------------");
+        }
     }
 }
