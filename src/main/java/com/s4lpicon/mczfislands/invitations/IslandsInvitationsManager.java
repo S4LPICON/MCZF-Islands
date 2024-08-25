4db241688c36f3b8d819c83881973e3c163fc2eb
Jeferson Pinzón <S4LPICONG4MER3993@GMAIL.COM>
2024-08-25 11:40:58 -0500

    administrador de invitaciones

---
+++ src/main/java/com/s4lpicon/mczfislands/invitations/IslandsInvitationsManager.java
@@ -0,0 +1,9 @@
+package com.s4lpicon.mczfislands.invitations;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class IslandsInvitationsManager {
+
+    //private HashMap<UUID, Invitation>
+}
